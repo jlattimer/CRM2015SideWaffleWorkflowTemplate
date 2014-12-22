19a92a39d85d17f85ae6c8fd0e61d274d1cd7a73
Jason Lattimer <jason.lattimer@gmail.com>
2014-12-21 19:36:51 -0600

    Initial check in

---
+++ CRM2015_7_0_X_WorkflowTemplate/ItemTemplates/YourName/Dynamics CRM/Workflow Class/WorkflowClass1.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Workflow;
+using System;
+using System.Activities;
+
+namespace $rootnamespace$
+{
+    public class $fileinputname$ : CodeActivity
+    {
+        private ITracingService _tracingService;
+        private IOrganizationService _service;
+
+        protected override void Execute(CodeActivityContext executionContext)
+        {
+            try
+            {
+                _tracingService = executionContext.GetExtension<ITracingService>();
+                IWorkflowContext context = executionContext.GetExtension<IWorkflowContext>();
+                IOrganizationServiceFactory serviceFactory = executionContext.GetExtension<IOrganizationServiceFactory>();
+                _service = serviceFactory.CreateOrganizationService(context.UserId);
+
+                //Do stuff
+            }
+            catch (Exception ex)
+            {
+                _tracingService.Trace("Exception: {0}", ex.ToString());
+            }
+        }
+    }
+}
